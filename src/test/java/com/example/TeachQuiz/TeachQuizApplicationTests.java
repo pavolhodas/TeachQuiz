8caa6059376e42806ed222f2c685ecb5d87c9cfe
pavolhodas <71751188+pavolhodas@users.noreply.github.com>
2022-10-06 17:17:58 +0200

    Add files via upload

---
+++ src/test/java/com/example/TeachQuiz/TeachQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeachQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
