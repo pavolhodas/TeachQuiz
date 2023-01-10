5465270afe8a2c383efcc5761c28f8b73de2d1bc
pavolhodas <pavolhodas4@gmail.com>
2023-01-10 17:49:05 +0100

    update get role to user

---
+++ src/main/java/com/example/TeachQuiz/resultEntity/ResultRepository.java
@@ -0,0 +1,4 @@
+package com.example.TeachQuiz.resultEntity;
+
+public interface ResultRepository {
+}
---
+++ src/main/java/com/example/TeachQuiz/resultEntity/ResultServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.TeachQuiz.resultEntity;
+
+public class ResultServiceImpl {
+}
